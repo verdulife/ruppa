c1d6323d7ca1c3df1aed28ae231f0c1d14da0687
verdulife <verdu@live.com>
2022-08-17 16:23:53 +0200

    initial

---
+++ src/lib/components/Preview.svelte
@@ -0,0 +1,8 @@
+<main>
+	<div class="video-wrapper">
+		<video controls />
+	</div>
+</main>
+
+<style lang="scss">
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Preview from '$lib/components/Preview.svelte';
+</script>
+
+<main class="viewport col">
+	<Preview />
+</main>
+
+<style lang="scss">
+	@import 'https://cdn.jsdelivr.net/gh/verdulife/utility-classes/dist/reset.css';
+	@import 'https://cdn.jsdelivr.net/gh/verdulife/utility-classes/dist/utility-classes.css';
+
+	.viewport {
+		background: #1b1b1b;
+	}
+</style>
